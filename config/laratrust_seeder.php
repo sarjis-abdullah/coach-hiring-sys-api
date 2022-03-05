a98d84b62e9f3c73004ee38f11b035652b1cbd1e
sarjis-abdullah <sarjis@reformedtech.org>
2022-03-05 17:02:33 +0600

    LaratrustSeeder added

---
+++ config/laratrust_seeder.php
@@ -0,0 +1,32 @@
+<?php
+
+return [
+    /**
+     * Control if the seeder should create a user per role while seeding the data.
+     */
+    'create_users' => false,
+
+    /**
+     * Control if all the laratrust tables should be truncated before running the seeder.
+     */
+    'truncate_tables' => true,
+
+    'roles_structure' => [
+        'admin' => [
+            'sport_types' => 'c,r,u,d',
+        ],
+        'coach' => [
+            'packages' => 'c,r,u,d'
+        ],
+        'athlete' => [
+            'packages' => 'r,u',
+        ]
+    ],
+
+    'permissions_map' => [
+        'c' => 'create',
+        'r' => 'read',
+        'u' => 'update',
+        'd' => 'delete'
+    ]
+];
